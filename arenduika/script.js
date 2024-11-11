954bb94cdeae2f0299b3ff16d7716a288af0fb3e
“Matsvei <matsvei.kazakevich@gmail.com>
2024-11-11 14:37:11 +0300

    feat: add adaptation for mobile

---
+++ arenduika/script.js
@@ -0,0 +1,4 @@
+document.getElementById('burger-menu').addEventListener('click', function() {
+    const navList = document.getElementById('nav-list');
+    navList.classList.toggle('active'); // Переключаем класс 'active'
+});
