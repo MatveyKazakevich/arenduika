b2e1dc6b1f90e101bcf5a7c85eeb8174722ab078
“Matsvei <matsvei.kazakevich@gmail.com>
2024-11-19 19:17:54 +0300

    feat: add mobile adaptation

--- arenduika/script.js
+++ arenduika/script.js
@@ -1,4 +1,28 @@
-document.getElementById('burger-menu').addEventListener('click', function() {
-    const navList = document.getElementById('nav-list');
-    navList.classList.toggle('active'); // Переключаем класс 'active'
-});
+function handleBurgerClick() {
+    const navbarEl = document.getElementById("navbar");
+    if (navbarEl.classList.contains("burgerView")) {
+      navbarEl.classList.remove("burgerView");
+    } else {
+      navbarEl.classList.add("burgerView");
+    }
+  
+    const burgerItemEl = document.getElementById("navbar-burger-item");
+    if (burgerItemEl.classList.contains("burgerView")) {
+      burgerItemEl.classList.remove("burgerView");
+    } else {
+      burgerItemEl.classList.add("burgerView");
+    }
+  }
+  
+  function ready() {
+    const navbarItemEls = document.getElementsByClassName("navbar-item");
+    for (let item of navbarItemEls) {
+      item.onclick = handleBurgerClick;
+    }
+  
+    const burgerIconEl = document.getElementById("navbar-burger-icon");
+    burgerIconEl.onclick = handleBurgerClick;
+  }
+  
+  document.addEventListener("DOMContentLoaded", ready);
+  
