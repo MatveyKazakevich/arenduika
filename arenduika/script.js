5e73614a85f9f53db13281e327f041d87ab018cf
“Matsvei <matsvei.kazakevich@gmail.com>
2024-11-24 22:16:40 +0300

    feat: add file connection

--- arenduika/script.js
+++ arenduika/script.js
@@ -1,4 +1,5 @@
-function handleBurgerClick() {
+  // Бургер меню //
+  function handleBurgerClick() {
     const navbarEl = document.getElementById("navbar");
     if (navbarEl.classList.contains("burgerView")) {
       navbarEl.classList.remove("burgerView");
@@ -13,16 +14,80 @@ function handleBurgerClick() {
       burgerItemEl.classList.add("burgerView");
     }
   }
+
+
+  // Подключение файлов //
+  const contentFilesById = {
+    about_us: "about.html",
+    catalog: "catalog.html",
+    autorize: "autorize.html",
+    recover: "recover.html",
+    register: "register.html",
+    acusticcat: "acusticcatal.html",
+    camping: "camping.html",
+    kidscat: "kidscatal.html",
+    powertools: "powertools.html",
+  };
+
+  const fetchContent = (id) => {
+    fetch(contentFilesById[id])
+      .then((response) => response.text())
+      .then((html) => (document.getElementById("content").innerHTML = html));
+  };
+  
+  const navigationHandleClick = (event) => {
+    const navbarLinkId = event.target.id;
+    fetchContent(navbarLinkId);
+    handleBurgerClick();
+  };
+
+  const handleForgotClick = () => {
+    fetchContent("recover");
+  }
+
+  const handleSignUpClick = () => {
+    fetchContent("register");
+  }
+
+  const handleKidsClick = () => {
+    fetchContent("kidscat");
+  }
+
+  const handlePowerToolsClick = () => {
+    fetchContent("powertools");
+  }
+
+  const handleAcusticClick = () => {
+    fetchContent("acusticcat");
+  }
+
+  const handleBackBtnClick = () => {
+    fetchContent("autorize");
+  }
+
+  const handleCampingClick = () => {
+    fetchContent("camping");
+  }
+
+  const handleBackBtn = () => {
+    fetchContent("catalog");
+  }
   
   function ready() {
-    const navbarItemEls = document.getElementsByClassName("navbar-item");
+    fetchContent("about_us"); // on initial page load
+    const navbarItemEls = document.getElementsByClassName("link");
     for (let item of navbarItemEls) {
+      item.onclick = navigationHandleClick;
+    }
+
+    const navbarItemEls1 = document.getElementsByClassName("navbar-item");
+    for (let item of navbarItemEls1) {
       item.onclick = handleBurgerClick;
     }
   
     const burgerIconEl = document.getElementById("navbar-burger-icon");
     burgerIconEl.onclick = handleBurgerClick;
   }
-  
+ 
   document.addEventListener("DOMContentLoaded", ready);
   
